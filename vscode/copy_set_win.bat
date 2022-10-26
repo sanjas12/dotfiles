dfb72fe16aa38ab62f22fcebd742a9f8d7502a62
sanja <sanja100v@gmail.com>
2022-10-26 23:31:39 +0300

    Update bat script

--- vscode/copy_set_win.bat
+++ vscode/copy_set_win.bat
@@ -1,5 +1,5 @@
- Copy-Item -f ./keybindings.json ~/AppData/Roaming/Code/User/
+Copy-Item .\keybindings.json $HOME\AppData\Roaming\Code\User\  
  
 @REM  copy ./keybindings.json $HOME/AppData/Roaming/Code/User/
 
- Copy-Item -f ./settings.json ~/AppData/Roaming/Code/User/co
+Copy-Item .\settings.json $HOME\AppData\Roaming\Code\User\
