e83b9385b9f2c69193af88b1376431f63c7c3195
sanja <sanja100v@gmail.com>
2022-10-26 16:35:36 +0300

    Add remote

--- vscode/copy_set_win.bat
+++ vscode/copy_set_win.bat
@@ -1,5 +1,5 @@
- cp -f ./keybindings.json ~/AppData/Roaming/Code/User/
+ Copy-Item -f ./keybindings.json ~/AppData/Roaming/Code/User/
  
 @REM  copy ./keybindings.json $HOME/AppData/Roaming/Code/User/
 
- cp -f ./settings.json ~/AppData/Roaming/Code/User/
+ Copy-Item -f ./settings.json ~/AppData/Roaming/Code/User/co
