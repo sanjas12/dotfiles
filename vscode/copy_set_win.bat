6b1f2a50bc62045261c703ebbbca15b12a090728
diakont_notebook <sanja100v@gmail.com>
2023-09-15 13:06:17 +0300

    VS CODE fix ProjectTemplates to win.bat

--- vscode/copy_set_win.bat
+++ vscode/copy_set_win.bat
@@ -1,8 +1,8 @@
-powershell -Command Copy-Item .\keybindings.json $HOME\AppData\Roaming\Code\User\  
+﻿powershell -Command Copy-Item .\keybindings.json $HOME\AppData\Roaming\Code\User\  
  
 @REM  copy ./keybindings.json $HOME/AppData/Roaming/Code/User/
 
 powershell -Command Copy-Item .\settings.json $HOME\AppData\Roaming\Code\User\
 
 @REM шаблоны для проектов по умолчанию
-powershell -Command Copy-Item .\vscode\ProjectTemplates ~\AppData\Roaming\Code\User -force -recurse
+powershell -Command Copy-Item .\ProjectTemplates ~\AppData\Roaming\Code\User -force -recurse
