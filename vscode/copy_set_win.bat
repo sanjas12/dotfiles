0a1b49648e0349900189e0c8e29101a5f4de0d49
diakont_notebook <sanja100v@gmail.com>
2023-09-15 13:03:30 +0300

    VS CODE add  ProjectTemplates to win.bat

--- vscode/copy_set_win.bat
+++ vscode/copy_set_win.bat
@@ -3,3 +3,6 @@ powershell -Command Copy-Item .\keybindings.json $HOME\AppData\Roaming\Code\User
 @REM  copy ./keybindings.json $HOME/AppData/Roaming/Code/User/
 
 powershell -Command Copy-Item .\settings.json $HOME\AppData\Roaming\Code\User\
+
+@REM шаблоны для проектов по умолчанию
+powershell -Command Copy-Item .\vscode\ProjectTemplates ~\AppData\Roaming\Code\User -force -recurse
